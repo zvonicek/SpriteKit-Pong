342e1e16795ad1c1f0620ce8f3ad0497d9514e3f
Petr Zvoníček <zvonicek@gmail.com>
2015-01-22 21:04:14 +0100

    Initial Commit

---
+++ TDT4240-pongTests/TDT4240_pongTests.swift
@@ -0,0 +1,36 @@
+//
+//  TDT4240_pongTests.swift
+//  TDT4240-pongTests
+//
+//  Created by Petr Zvoníček on 22.01.15.
+//  Copyright (c) 2015 Petr Zvoníček. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TDT4240_pongTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
